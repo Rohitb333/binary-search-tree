df1b5cc960793440116e1d7a920fcde0a0dc2c69
Rohit <rohitb333@gmail.com>
2022-03-28 16:09:47 +0530

    Added Display PreOrder Method

--- src/com/bridelabz/BinaryTreeClass.java
+++ src/com/bridelabz/BinaryTreeClass.java
@@ -45,6 +45,18 @@ public class BinaryTreeClass {
         preorder(root.right);
     }
 
+    public static void inorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + "  ");
+        inorder(root.right);
+    }
+
+
 
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -56,6 +68,10 @@ public class BinaryTreeClass {
         System.out.println("==================================");
         System.out.println("Display Preorder");
         preorder(root);
+        System.out.println();
+        System.out.println("==================================");
+        System.out.println("Display Inorder");
+        inorder(root);
         System.out.println();
         System.out.println("==================================");
 
